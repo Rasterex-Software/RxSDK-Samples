22461a515304c68488724635099ebfbb75098245
Terje Helgesen <66789967+TerjeHelgesen-rx@users.noreply.github.com>
2020-10-20 00:17:15 +0200

    Update Program.cs

--- rxPublishWebEx/rxPublishWebEx/Program.cs
+++ rxPublishWebEx/rxPublishWebEx/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,17 +13,23 @@ namespace rxPublishWebEx
    {
       static void Main(string[] args)
       {
+         //Default publish options:
+         RXCONVERTCOMLib.RX_WEBPUBLISH_OPTIONS options = RXCONVERTCOMLib.RX_WEBPUBLISH_OPTIONS.RX_WEB_NOPRINTIMAGES | RXCONVERTCOMLib.RX_WEBPUBLISH_OPTIONS.RX_WEB_VECTOREMBEDIMAGES;
+
          RxConverter myRxConverter = new RxConverter();
          RxEngine myRxEngine = new RxEngine();
          myRxEngine.Start(RXDOCCOMLib.RX_REGISTRY_KEY.RX_REGKEY_LOCAL_MACHINE, "SOFTWARE\\Rasterex\\RxFilters");
 
          if (args.Count() == 0)
          {
-            Console.WriteLine("Usage:\rxPublishWebEx.exe inputfile outputfolder");
+            Console.WriteLine("Usage:\nrxPublishWebEx.exe inputfile outputfolder");
+            Console.WriteLine("Optional usage:\nrxPublishWebEx.exe inputfile outputfolder /nocache");
          }
          else
          {
-            myRxConverter.PublishWebEx(args[1], args[0], 0);
+            if ( args.Count()> 2 && args[2].Equals( "/nocache", StringComparison.OrdinalIgnoreCase) )
+               options |= RXCONVERTCOMLib.RX_WEBPUBLISH_OPTIONS.RX_WEB_NOCACHEPATH;
+            myRxConverter.PublishWebEx(args[1], args[0], options );
          }
 
          myRxEngine.Stop();
